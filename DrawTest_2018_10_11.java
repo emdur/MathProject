63e86c1c01818c011b922bdff7b6a3d51770da20
emdur <45200967+emdur@users.noreply.github.com>
2018-12-03 11:03:29 +0100

    Add files via upload

--- DrawTest_2018_10_11.java
+++ DrawTest_2018_10_11.java
@@ -19,43 +19,49 @@ public class DrawTest_2018_10_11 extends JFrame {
 		DrawTest_2018_10_11 testObjekt = new DrawTest_2018_10_11();
 
 		testObjekt.setVisible(true);
+		double absT = 0;
 		while (true) {
-			testObjekt.draw();
+			testObjekt.draw(absT);
 
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(1000); // in Millisekunden
+				absT += 7;
 			} catch (InterruptedException e) {
 			}
 		}
 
 	}
 
-	void draw() {
+	// Wir müssen auf jeden Fall die Parameter g, h, s, r aus der Lanchester-Klasse
+	// in diese Methode stopfen
+	void draw(double absT) {
 		Graphics g = getGraphics();
-
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
-		g.setColor(Color.RED);
-		g.fillOval(Constants.WINDOW_WIDTH / 15, Constants.WINDOW_HEIGHT / 15, 5, 5);
 
+		int x = 0;
+		int y = 0;
+		// Schleife für die Erstellung der Anfangspopulation g
+		for (int i = 0; i < 500 /* Hier muss iwie Lanchester.g hin */; i++) {
+			g.setColor(Color.RED);
+			g.fillOval(Constants.WINDOW_WIDTH / 15 + x, Constants.WINDOW_HEIGHT / 15 + y, 5, 5);
+
+			// x und y mit if, damit die Punkte sich nicht überlagern und links oben
+			// erscheinen
+			// Die Punkte von h könnten dann in eine der anderen Ecken, ich wollt nur erst
+			// mal die
+			// Animation von g hinkriegen, h wird ja dann ähnlich.
+			x += 6;
+			if (x > 300) {
+				y += 6;
+				x = 0;
+			}
+		}
+		
+		//Schleife für die Animation der Abnahme der Population beim Kampf
+		for(int i = 0; i <= /*LanchesterMethods.wannistderkampfentschieden (also tdeath)*/; i++) {
+		
+			// LanchesterMethods.populationt(g, h, s, r, i) --> i ist der Zeitparameter t der Methode, die z.B. G(t) ausrechnet
+		}
 	}
-	/*
-	 * void draw() { Graphics g = getGraphics();
-	 * 
-	 * g.setColor(Color.RED); g.fillRect(0, 0, Constants.WINDOW_WIDTH,
-	 * Constants.WINDOW_HEIGHT); g.setColor(Color.BLACK);
-	 * g.drawLine(Constants.WINDOW_WIDTH / 2, 0, Constants.WINDOW_WIDTH / 2,
-	 * Constants.WINDOW_HEIGHT); g.drawLine(0, Constants.WINDOW_HEIGHT / 2,
-	 * Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT / 2);
-	 * g.setColor(Color.GREEN); g.drawLine(Constants.WINDOW_WIDTH * 3 / 8,
-	 * Constants.WINDOW_HEIGHT / 2, Constants.WINDOW_WIDTH / 2,
-	 * Constants.WINDOW_HEIGHT / 2 - (int) (Math.sqrt(3) * Constants.WINDOW_WIDTH *
-	 * 1 / 8)); g.drawLine(Constants.WINDOW_WIDTH * 5 / 8, Constants.WINDOW_HEIGHT /
-	 * 2, Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT / 2 - (int)
-	 * (Math.sqrt(3) * Constants.WINDOW_WIDTH * 1 / 8));
-	 * g.drawLine(Constants.WINDOW_WIDTH * 3 / 8, Constants.WINDOW_HEIGHT / 2,
-	 * Constants.WINDOW_WIDTH * 5 / 8, Constants.WINDOW_HEIGHT / 2);
-	 * g.setColor(Color.BLUE); g.fillOval(Constants.WINDOW_WIDTH / 4,
-	 * Constants.WINDOW_HEIGHT / 2, 100, 100); }
-	 */
 }
