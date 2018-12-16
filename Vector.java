28d72fcf7fb5739e78fbeb96e08d7986bbf7e933
emdur <45200967+emdur@users.noreply.github.com>
2018-12-16 03:44:28 +0100

    Add files via upload

--- Vector.java
+++ Vector.java
@@ -3,12 +3,14 @@ package app;
 public class Vector {
 
 	// Konstruktor für Punkt x y
-	private int xKo;
-	private int yKo;
+	int xKo;
+	int yKo;
+	boolean visible;
 
-	Vector(int xKo, int yKo) { // Visibility: package
+	Vector(int xKo, int yKo, boolean visible) { // Visibility: package
 		this.xKo = xKo;
 		this.yKo = yKo;
+		this.visible = visible;
 	};
 
 }
