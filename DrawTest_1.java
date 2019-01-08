dd01f5bae5196ea687389a3d7e611aa1435b1ad6
emdur <45200967+emdur@users.noreply.github.com>
2019-01-08 14:46:49 +0100

    Add files via upload

--- DrawTest_1.java
+++ DrawTest_1.java
@@ -22,8 +22,8 @@ public class DrawTest_1 extends JFrame {
 
 	static List<Vector> PopListG = new ArrayList<Vector>();
 	static List<Vector> PopListH = new ArrayList<Vector>();
-	static Population g1 = new Population(600, 3);
-	static Population h1 = new Population(500, 6);
+	static Population g1 = new Population(300, 1);
+	static Population h1 = new Population(200, 6);
 
 	public static void main(String[] args) {
 
@@ -63,6 +63,7 @@ public class DrawTest_1 extends JFrame {
 
 	// int f = 0;
 	int n = 0;
+	Vector vec;
 
 	void drawPopulation(Graphics g, Population p, Population p2, int leftright, int popcolor, List<Vector> PopList,
 			double tcounter, double d) {
@@ -70,8 +71,6 @@ public class DrawTest_1 extends JFrame {
 		// TOTE PUNKTE VON DER LISTE LÖSCHEN
 		// berechne Anzahl n zu löschender Punkte
 
-		// n = Populationsgröße des letzten Frames - aktuelle Populationsgröße
-		n = Population.populationt(p, p2, tcounter - d) - Population.populationt(p, p2, tcounter);
 		// System.out.println(n);
 		// f = n;
 
@@ -80,26 +79,39 @@ public class DrawTest_1 extends JFrame {
 		// System.out.println("1: " + Population.populationt(p, p2, tcounter));
 		// System.out.println("2: " + Population.populationt(p, p2,
 		// Population.wannistderkampfentschieden(p, p2)));
-		if (Population.populationt(p, p2, tcounter) > Population.populationt(p, p2,
-				Population.wannistderkampfentschieden(p, p2))) {
-			// PopList an random Stellen löschen
-			if (PopList.size() <= 1) {
-				PopList.clear();
-			} else {
-				Random ran = new Random();
-				int r;
-				int s;
-				for (int l = 0; l < n; l++) {
-					if (PopList.size() >= 1) {
-						// random number from 0 to PopListSize-1 --> n random Stellen
-						r = (int) (Math.random() * PopList.size());
-						System.out.println("r: " + r);
-						PopList.remove(r);
-					}
+		// (Population.populationt(p, p2, tcounter)
+		// if (PopList.size() > Population.populationt(p, p2,
+		// Population.wannistderkampfentschieden(p, p2))) {
 
-				}
+		n = Population.populationt(p, p2, tcounter - d) - Population.populationt(p, p2, tcounter);
+		Random ran = new Random();
+		int r = 0;
+		int s;
+		// if (PopList.size() > Population.populationt(p, p2,
+		// Population.wannistderkampfentschieden(p, p2))) {
+		// try {
+
+		for (int l = 0; l < n; l++) {
+
+			try {
+				// random number from 0 to PopListSize-1 --> n random Stellen
+				r = (int) (Math.random() * PopList.size());
+				// System.out.println("r: " + r);
+				vec = PopList.get(r);
+				PopList.remove(r);
+			} catch (IndexOutOfBoundsException e) {
+				// System.out.println("This is the ennnnnd");
+				// PopList.add(r, vec);
 			}
-		} // Ende if
+		}
+
+		// }
+		// } // Ende if
+		drawOnly(popcolor, g, PopList);
+
+	}
+
+	void drawOnly(int popcolor, Graphics g, List<Vector> PopList) {
 
 		// AUSLESEN DER LISTEN UND ZEICHNEN DER PUNKTE IM AKTUELLEN FRAME
 		// Es malt jz weiße Punkte, statt welche ganz zu löschen(edit: es malt sie jz
@@ -112,12 +124,11 @@ public class DrawTest_1 extends JFrame {
 		if (popcolor == 1) {
 			g.setColor(Color.BLUE);
 		}
-		for (int j = 0; j < PopList.size() - 1; j++) {
+		for (int j = 0; j < PopList.size(); j++) {
 			Vector v = PopList.get(j);
 			// draw point
 			g.fillOval(v.xKo, v.yKo, 5, 5);
 		}
-
 	}
 
 	double tcounter = 0;
@@ -135,13 +146,27 @@ public class DrawTest_1 extends JFrame {
 
 		int leftright = 0;
 		int popcolor = 0;
-		// g1.size = Population.populationt(g1, h1, tcounter);
-		drawPopulation(g, g1, h1, leftright, popcolor, PopListG, tcounter, d);
+
+		// double td = Population.populationt(g1, h1,
+		// Population.wannistderkampfentschieden(g1, h1));
+		if (PopListG.size() > Population.populationt(g1, h1, Population.wannistderkampfentschieden(g1, h1))) {
+			// g1.size = Population.populationt(g1, h1, tcounter);
+			drawPopulation(g, g1, h1, leftright, popcolor, PopListG, tcounter, d);
+		} else {
+			drawOnly(popcolor, g, PopListG);
+		}
 
 		popcolor = 1;
 		leftright = Constants.WINDOW_WIDTH / 2;
-		// h1.size = Population.populationt(h1, g1, tcounter);
-		drawPopulation(g, h1, g1, leftright, popcolor, PopListH, tcounter, d);
+		// double tdd = Population.populationt(h1, g1,
+		// Population.wannistderkampfentschieden(h1, g1));
+		if (PopListH.size() > Population.populationt(h1, g1, Population.wannistderkampfentschieden(h1, g1))) {
+
+			// h1.size = Population.populationt(h1, g1, tcounter);
+			drawPopulation(g, h1, g1, leftright, popcolor, PopListH, tcounter, d);
+		} else {
+			drawOnly(popcolor, g, PopListH);
+		}
 
 		tcounter += 0.01;
 		d = tcounter - 0.01;
